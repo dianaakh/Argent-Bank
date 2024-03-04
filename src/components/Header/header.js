c11b34b0f0b93cd359886f1aebdf7de5c80c3340
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-03-04 23:02:58 +0100

    adds component header + style

---
+++ src/components/Header/header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/argentBankLogo.png';
+import './header.css';
+
+const Header = () => {
+    return (
+      <nav className='main-nav'>
+        <NavLink to='/' className='main-nav-logo'>
+          <img src={logo} alt='Argent Bank Logo' className='main-nav-logo-image' />
+          <h1 className='sr-only'>Argent Bank</h1>
+        </NavLink>
+        <div>
+          <NavLink to='/login' className='main-nav-item'>
+            <i className='fa fa-user-circle'></i>
+            Sign In
+          </NavLink>
+        </div>
+      </nav>
+    );
+  };
+
+export default Header;
