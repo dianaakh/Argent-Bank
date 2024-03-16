84e9944b59e7b68eadea3a53afec64ce679ed993
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-03-16 14:54:10 +0100

    adds page component Login

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/header';
+import Footer from '../components/Footer/footer';
+import Form from '../components/Form/form';
+
+const Login = () => {
+  return (
+    <div className='page_login'>
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
