f462ccfa8463147ac73fa48441b23ed6c0c1282a
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-03-27 13:46:18 +0100

    modify img extensions

--- src/data/featuresData.js
+++ src/data/featuresData.js
@@ -1,6 +1,6 @@
-import iconChat from '../assets/icon-chat.png';
-import iconMoney from '../assets/icon-money.png';
-import iconSecurity from '../assets/icon-security.png';
+import iconChat from '../assets/icon-chat.webp';
+import iconMoney from '../assets/icon-money.webp';
+import iconSecurity from '../assets/icon-security.webp';
 
 const featuresData = [
     {
